bf4f9a61bcb313ede98b21a5a8bbcff00a4bf88a
Firo <joshcole1331@gmail.com>
2024-04-29 20:09:45 -0300

    Add prelim  models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Meal(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={
+            'meal_id': self.id
+        })
+
+class Comment(models.Model):
+    user = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f'{self.user} ({self.id})'
+    
