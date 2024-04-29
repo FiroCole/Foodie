7a3ed3a89230ca9f06751388f45af306cf24474d
Firo <joshcole1331@gmail.com>
2024-04-29 20:00:05 -0300

    Add class-based views for Meals/Comments

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Meal, Comment
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
 # Define the home view
@@ -7,10 +11,57 @@ def home(request):
   return render(request, 'home.html')
 
 def about(request):
-    return(request, 'about.html')
+    return render(request, 'about.html')
 
 def meals_index(request):
   # We pass data to a template very much like we did in Express!
   return render(request, 'meals/index.html', {
     'meals': meals
   })
+
+# CBV for Meals
+class MealList(ListView):
+  model = Meal
+
+class MealDetail(DetailView):
+    model = Meal
+
+class MealCreate(CreateView):
+    model = Meal
+    fields = '__all__'
+
+class MealUpdate(UpdateView):
+    model = Meal
+    fields = '__all__'
+
+class MealDelete(DeleteView):
+    model = Meal
+    success_url = '/meals'
+
+# CBV for Comments
+class CommentList(ListView):
+  model = Comment
+
+class CommentDetail(DetailView):
+    model = Comment
+
+class CommentCreate(CreateView):
+    model = Comment
+    fields = '__all__'
+
+class CommentUpdate(UpdateView):
+    model = Comment
+    fields = '__all__'
+
+class CommentDelete(DeleteView):
+    model = Comment
+    success_url = '/comments'
+    
+
+
+
+
+
+def assoc_comment(request, meal_id, comment_id):
+  Meal.objects.get(id=meal_id).comments.add(comment_id)
+  return redirect('detail', meal_id=meal_id)
